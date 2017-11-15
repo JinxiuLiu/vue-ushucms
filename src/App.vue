42c914952f5347a4fcf9c889d9b201586bbc7806
zhoumo <zhoumo@sayweb.top>
2017-11-15 18:00:46 +0800

    init && add navmenu

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="app">
+        <div class="header"></div>
+        <div class="navmenu">
+            <NavMenu></NavMenu>
+        </div>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+    import NavMenu from './components/NavMenu';
+    export default {
+        name: 'app',
+        components: {
+            NavMenu
+        },
+    }
+</script>
+
+<style lang='less'>
+    #app {
+        width: 100%;
+        height: 100%;
+        background: #e7ebf3;
+        .header {
+            width: 100%;
+            height: 88px;
+            background: #41403f;
+        }
+        .navmenu {
+            width: 182px;
+            height: 100%;
+            border-right: 1px solid #d2dae9;
+            position: fixed;
+            top: 88px;
+            .el-row {
+                height: 100%;
+            }
+            .el-col-24 {
+                height: 100%;
+            }
+            .el-menu {
+                height: 100%;
+                border-right: none;
+            }
+            .el-submenu .el-menu-item {
+                min-width: 182px;
+            }
+        }
+    }
+</style>
---
+++ src/components/NavMenu.vue
@@ -0,0 +1,42 @@
+<template>
+    <el-row class="tac">
+        <el-col>
+            <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" background-color="#545c64" text-color="#fff" active-text-color="#30b8fe">
+                <el-menu-item index="1">
+                    <i class="el-icon-picture"></i>
+                    <span>轮播图片</span>
+                </el-menu-item>
+                <el-menu-item index="2">
+                    <i class="el-icon-sold-out"></i>
+                    <span slot="title">折扣设置</span>
+                </el-menu-item>
+                <el-menu-item index="3">
+                    <i class="el-icon-success"></i>
+                    <span slot="title">身份认证</span>
+                </el-menu-item>
+                <el-menu-item index="4">
+                    <i class="el-icon-info"></i>
+                    <span slot="title">提现审核</span>
+                </el-menu-item>
+                <el-menu-item index="5">
+                    <i class="el-icon-document"></i>
+                    <span slot="title">订单信息</span>
+                </el-menu-item>
+            </el-menu>
+        </el-col>
+    </el-row>
+</template>
+<script>
+    export default {
+        name: 'NavMenu',
+        methods: {
+            handleOpen(key, keyPath) {
+                console.log(key, keyPath);
+            },
+            handleClose(key, keyPath) {
+                console.log(key, keyPath);
+            }
+        }
+    }
+
+</script>

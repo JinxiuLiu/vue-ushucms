c59d24a417b9be51dcd786c52ab9bf36b3d0e337
zhoumo <zhoumo@sayweb.top>
2017-11-16 18:06:26 +0800

    Add Carousel

--- src/containers/Carousel/Carousel.vue
+++ src/containers/Carousel/Carousel.vue
@@ -1,28 +1,91 @@
 <template>
-	<div class="title">轮播图设置</div>
+    <div class="carousel">
+        <div class="title"><span class="text">轮播图设置</span></div>
+        <div class="info">
+            <el-row>
+                <el-col :span="12">
+                    <template>
+                        <el-table :data="tableData" :show-header="false" style="width: 100%">
+                            <el-table-column align="left">
+                                <template slot-scope="scope">
+                                    <span>{{ scope.row.bannerName }}</span>
+                                </template>
+                            </el-table-column>
+                            <el-table-column align="right">
+                                <template slot-scope="scope">
+                                    <div class="ushu-column">
+                                    	<el-switch v-model="scope.row.cheacked" active-color="#409EFF" inactive-color="#ff4444"></el-switch>
+                                    	<el-button type="text" @click="handleAffirm(scope.$index, scope.row)" icon="el-icon-delete"></el-button>
+                                    </div>
+                                </template>
+                            </el-table-column>
+                        </el-table>
+                    </template>
+                </el-col>
+                <el-col :span="12">
+                </el-col>
+            </el-row>
+        </div>
+    </div>
 </template>
 
 <script>
 	export default {
-        name: 'Carousel',
-        data() {
-            return {
-                
-            }
-        },
-        methods: {
-            
-        }
-    }
+	    name: 'Carousel',
+	    data() {
+	        return {
+	        	cheacked: true,
+	            tableData: [{
+	                bannerName: '王小虎',
+	                cheacked: true,
+	            }, {
+	                bannerName: '王小虎',
+	                cheacked: true,
+	            }, {
+	                bannerName: '王小虎',
+	                cheacked: true,
+	            }, ]
+	        }
+	    },
+	    methods: {
+
+	    }
+	}
+
 </script>
 
 <style lang='less' scoped>
-	.title {
-		width: 100%;
-		height: 40px;
-		line-height: 40px;
-		padding-left: 20px;
-		background: #f7f8f8;
-		font-size: 14px;
+	.carousel {
+	    background: #fff;
+	    width: 100%;
+	    min-height: 100%;
+	    .title {
+	        width: 100%;
+	        height: 40px;
+	        line-height: 40px;
+	        background: #f7f8f8;
+	        font-size: 14px;
+	        .text {
+	            padding-left: 20px;
+	        }
+	    }
+	    .info {
+	        font-size: 14px;
+	        padding: 0 20px;
+	        .el-table--enable-row-hover .el-table__body tr:hover>td {
+	            background-color: #fff;
+	        }
+	        .el-button--text {
+	        	width: 40px;
+	        	font-size: 20px;
+	        	color: #666;
+	        	padding: 0;
+	        }
+	        .ushu-column {
+	        	display: flex;
+	        	justify-content: flex-end;
+	        }
+	    }
 	}
+
 </style>
--- src/containers/Discount/Discount.vue
+++ src/containers/Discount/Discount.vue
@@ -1,5 +1,20 @@
 <template>
-	<div class="title">折扣</div>
+	<div class="discount">
+		<div class="title"><span class="text">折扣</span></div>
+		<div class="info">
+			<div class="info-input">
+				<span class="input-tip">折扣：</span>
+				<template>
+					<el-input-number v-model="input" @change="handleChange" :min="1.0" :max="10.0" :controls="false" label=""></el-input-number>
+				</template>
+				<span class="input-tip">折</span>
+				<span class="tip">*此折扣适用于所有图书</span>
+			</div>
+			<div class="btn">
+				<el-button type="primary" size="medium">　　保存　　</el-button>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -7,7 +22,7 @@
         name: 'Discount',
         data() {
             return {
-                
+                input: '9.9'
             }
         },
         methods: {
@@ -17,12 +32,34 @@
 </script>
 
 <style lang='less' scoped>
-	.title {
+	.discount {
+		background: #fff;
 		width: 100%;
-		height: 40px;
-		line-height: 40px;
-		padding-left: 20px;
-		background: #f7f8f8;
-		font-size: 14px;
+		min-height: 100%;
+		.title {
+			width: 100%;
+			height: 40px;
+			line-height: 40px;
+			background: #f7f8f8;
+			font-size: 14px;
+			.text {
+				padding-left: 20px;
+			}
+		}
+		.info {
+			font-size: 14px;
+			padding: 30px 20px;
+			.el-input-number {
+				width: 120px;
+			}
+			.tip {
+				padding-left: 20px;
+				color: #999;
+			}
+			.btn {
+				margin-top: 20px;
+			}
+		}
 	}
+
 </style>
